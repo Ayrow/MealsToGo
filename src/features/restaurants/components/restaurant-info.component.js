6a99d4326567159bf3ba52ab74842de860a20108
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-04 11:47:31 +0100

    set some dummy data for restaurant info

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,17 @@
 import { Text } from 'react-native';
 
-const RestaurantInfo = () => {
-  return <Text>Restaurant info</Text>;
+const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = 'some restaurant',
+    icon,
+    photos = [
+      'https://cdn.sortiraparis.com/images/1004/91874/582754-photos-le-cafe-de-l-homme.jpg',
+    ],
+    address = '100 some random street',
+    isOpenNowe = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
 };
 export default RestaurantInfo;
