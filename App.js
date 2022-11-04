e570375bd23e9b248632b631d52047ba52263f95
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-04 11:39:41 +0100

    moved codes to different components

--- App.js
+++ App.js
@@ -1,14 +1,5 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-} from 'react-native';
-
-import { Searchbar } from 'react-native-paper';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
   // const isAndroid = Platform.OS === 'android';
@@ -18,35 +9,8 @@ export default function App() {
 
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder='Search'
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style='auto' />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: 'green',
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,6 @@
+import { Text } from 'react-native';
+
+const RestaurantInfo = () => {
+  return <Text>Restaurant info</Text>;
+};
+export default RestaurantInfo;
