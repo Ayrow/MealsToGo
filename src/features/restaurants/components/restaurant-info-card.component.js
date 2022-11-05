8227d53b28235d57ea3afbd851c64f8194a3fd08
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-05 11:45:03 +0100

    migrated restaurant card to styled components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,9 +1,18 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text } from 'react-native';
 import styled from 'styled-components/native';
 import { Card } from 'react-native-paper';
 
-const Title = styled.Text`
+const Title = styled(Text)`
   padding: 16px;
+`;
+
+const RestaurantCard = styled(Card)`
+  background-color: 'white';
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 16px;
+  background-color: 'white';
 `;
 
 const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -19,16 +28,11 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: 'white' },
-  cover: { padding: 20, backgroundColor: 'white' },
-});
 
 export default RestaurantInfoCard;
