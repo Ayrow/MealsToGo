de20f34e535d3496998ab15508e819263f44c6dc
Ayrow <aymeric.pilaert@hotmail.fr>
2022-11-06 12:30:44 +0100

    added rating to restaurant card

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Text } from 'react-native';
 import styled from 'styled-components/native';
 import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+
+import star from '../../../../assets/star';
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -27,6 +30,12 @@ const Info = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Rating = styled(View)`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = 'some restaurant',
@@ -39,11 +48,19 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+        <rating>
+          {ratingArray.map((item) => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </rating>
         <Address>{address}</Address>
       </Info>
     </RestaurantCard>
